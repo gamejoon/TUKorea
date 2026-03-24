913189dad0ac53989053ee36880c69cccb602b95
gamejoon <gamejoon0325@naver.com>
2026-03-24 17:34:33 +0900

    Study 26/03/24

--- 26-1/STL/월56화78/메인.cpp
+++ 26-1/STL/월56화78/메인.cpp
@@ -1,54 +1,52 @@
 ﻿//-------------------------------------------------------------------
-// 2026년 1학기 STL 월56화78			3월 23일				(3주 2일)
+// 2026년 1학기 STL 월56화78			3월 24일				(4주 1 일)
 //-------------------------------------------------------------------
-// 많은 수의 자료를 다루기 - FILE I/O - binary I/O - class 객체
+// 스마트 포인터 -> 콜러블 타입 -> 실습 -> STL
 //-------------------------------------------------------------------
 #include <iostream>
-#include <fstream>
-#include <array>
-#include <print>
-#include <string>
+#include <numeric>
+#include <memory>
 #include "save.h"
 
-class Dog {
-private:
-	std::string name;
-	size_t id;
-
-	friend std::istream& operator >> (std::istream& is, const Dog& dog) {
-		is.read((char*)&dog, sizeof(Dog));
-		return is;
-	}
-
-	friend std::ostream& operator << (std::ostream& os, const Dog& dog) {
-		std::print(os, "[{:>4}] - {}", dog.id, dog.name);
-		return os;
-	}
-};
-
-// [문제] 파일 "Dog천마리"에는 class Dog 객체 1000개가 저장되어있다.
-// 파일은 binary mode이고 각 객체는 메모리 크기 그대로 string의 write 함수로 기록하였다.
-// 모든 객체는 한번의 write 함수를 사용하여 기록하였다.
-// Dog의 멤버는 위의 코드와 같다.
-// 메모리에 모두 읽어 와라.
-// 메모리에 있는 모든 Dog를 화면에 출력하라.
+// [문제] 사용자가 입력한 수만큼 int를 저장할 메모리를 확보하라.
+// 1부터 시작하는 정수로 메모리를 채워라.
+// 합계를 화면에 출력하라.
 
 //--------
 int main()
 //--------
 {
-	std::ifstream in{ "Dog천마리", std::ios::binary };
-	if (not in) {
-		std::cout << "Dog 없음" << std::endl;
-		return 20260323;
+	//int* p; -> 사용하지 말아야 함
+	// C++11(Modern C++)에 이것의 완벽한 대체 수단이 있기 때문에 -> smart_point
+	
+	std::unique_ptr<int[]> p;
+	while (true) {
+		std::cout << "int 몇 개가 필요하신지? ";
+		size_t num;
+		std::cin >> num;
+
+		try {
+			p.reset( new int[num] );
+		}
+		catch (std::exception& e) {
+			std::cout << "메모리가 고갈 - " << e.what() << std::endl;
+		}
+
+		//for (int i = 0; i < num; ++i) {
+		//	p[i] = i + 1;
+		//}
+		std::iota(p.get(), p.get() + num, 1);
+
+		//long long sum{};
+		//for (int i = 0; i < num; ++i) {
+		//	sum += p[i];
+		//}
+
+		long long sum = std::accumulate(p.get(), p.get() + num, 0LL); // 접미사 long long
+		std::cout << "1 부터 " << num << "까지의 합계 ----- " << sum << std::endl; // 가산한다
+
+		//delete[] p; 더이상 내가 직접 메모리 반환을 안해도 됨
 	}
 
-	std::array<Dog, 1000> dogs;
-
-	in.read((char*)dogs.data(), dogs.size() * sizeof(Dog));
-
-	for (const Dog& dog : dogs) {
-		std::cout << dog << std::endl;
-	}
 	save("메인.cpp");
 }
